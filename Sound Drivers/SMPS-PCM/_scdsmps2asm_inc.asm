c2efa9b882810b42906e421300f308548d720b44
Liu Wenyuan <15816141883@163.com>
2022-08-05 12:03:20 +0800

    Remove SMPS-68k remeants

--- Sound Drivers/SMPS-PCM/_scdsmps2asm_inc.asm
+++ Sound Drivers/SMPS-PCM/_scdsmps2asm_inc.asm
@@ -3,9 +3,6 @@
 ; SCD mod by Dobby233Liu
 ; ====================================================================================================
 
-; PSG conversion to S3/S&K/S3D drivers require a tone shift of 12 semi-tones.
-psgdelta	EQU 12
-; ---------------------------------------------------------------------------------------------
 ; Standard Octave Pitch Equates
 smpsPitch10lo	EQU $88
 smpsPitch09lo	EQU $94
@@ -126,44 +123,6 @@ nG7			EQU	$DC
 nAb7		EQU	$DD
 nA7			EQU	$DE
 nBb7		EQU	$DF
-; SMPS2ASM uses nMaxPSG for songs from S1/S2 drivers.
-; nMaxPSG1 and nMaxPSG2 are used only for songs from S3/S&K/S3D drivers.
-; The use of psgdelta is intended to undo the effects of PSGPitchConvert
-; and ensure that the ending note is indeed the maximum PSG frequency.
-nMaxPSG				EQU nBb6-psgdelta
-nMaxPSG1			EQU nBb6
-nMaxPSG2			EQU nB6
-; ---------------------------------------------------------------------------------------------
-; PSG Flutter Equates
-fTone_00	EQU	$00
-fTone_01	EQU	$01
-fTone_02	EQU	$02
-fTone_03	EQU	$03
-fTone_04	EQU	$04
-fTone_05	EQU	$05
-fTone_06	EQU	$06
-fTone_07	EQU	$07
-fTone_08	EQU	$08
-fTone_09	EQU	$09
-fTone_0A	EQU	$07
-fTone_0B	EQU	$05
-fTone_0C	EQU	$00
-fTone_0D	EQU	$00
-sTone_01	EQU	$02
-sTone_02	EQU	$02
-sTone_03	EQU	$05
-sTone_04	EQU	$03
-sTone_05	EQU	$01
-sTone_06	EQU	$03
-sTone_07	EQU	$00	; SFX envelope, probably unused in S3K
-sTone_08	EQU	$09
-sTone_09	EQU	$05
-sTone_0A	EQU	$07
-sTone_0B	EQU	$00	; For FM volume envelopes
-sTone_0C	EQU	$09
-sTone_0D	EQU	$00	; This time it matches 100%
-sTone_0E	EQU	$02	; Duplicate of 01
-sTone_0F	EQU	$02	; Duplicate of 02
 ; ---------------------------------------------------------------------------------------------
 ; Channel IDs for SFX
 cPCM1				EQU $00
@@ -197,7 +156,7 @@ tempoDivider set div
 	dc.b    mod
 	endm
 
-; Header - Set up FM Channel
+; Header - Set up PCM Channel
 smpsHeaderPCM macro loc,pitch,vol
 	dc.w	loc-songStart
 	dc.b	pitch,vol
@@ -320,130 +279,4 @@ smpsChanTempoDiv macro val
 ; FBxx - Set music tick multiplier to xx
 smpsSetTempoDiv macro val
 	dc.b	$EB,val
-	endm
-; ---------------------------------------------------------------------------------------------
-; S1/S2 only coordination flag
-; Sets D1L to maximum volume (minimum attenuation) and RR to maximum for operators 3 and 4 of FM1
-smpsWeirdD1LRR macro
-	dc.b	$F9
-	endm
-; ---------------------------------------------------------------------------------------------
-; Macros for FM instruments
-; Voices - Feedback
-smpsVcFeedback macro val
-vcFeedback set val
-	endm
-
-; Voices - Algorithm
-smpsVcAlgorithm macro val
-vcAlgorithm set val
-	endm
-
-smpsVcUnusedBits macro val
-vcUnusedBits set val
-	endm
-
-; Voices - Detune
-smpsVcDetune macro op1,op2,op3,op4
-vcDT1 set op1
-vcDT2 set op2
-vcDT3 set op3
-vcDT4 set op4
-	endm
-
-; Voices - Coarse-Frequency
-smpsVcCoarseFreq macro op1,op2,op3,op4
-vcCF1 set op1
-vcCF2 set op2
-vcCF3 set op3
-vcCF4 set op4
-	endm
-
-; Voices - Rate Scale
-smpsVcRateScale macro op1,op2,op3,op4
-vcRS1 set op1
-vcRS2 set op2
-vcRS3 set op3
-vcRS4 set op4
-	endm
-
-; Voices - Attack Rate
-smpsVcAttackRate macro op1,op2,op3,op4
-vcAR1 set op1
-vcAR2 set op2
-vcAR3 set op3
-vcAR4 set op4
-	endm
-
-; Voices - Amplitude Modulation
-smpsVcAmpMod macro op1,op2,op3,op4
-vcAM1 set op1
-vcAM2 set op2
-vcAM3 set op3
-vcAM4 set op4
-	endm
-
-; Voices - First Decay Rate
-smpsVcDecayRate1 macro op1,op2,op3,op4
-vcD1R1 set op1
-vcD1R2 set op2
-vcD1R3 set op3
-vcD1R4 set op4
-	endm
-
-; Voices - Second Decay Rate
-smpsVcDecayRate2 macro op1,op2,op3,op4
-vcD2R1 set op1
-vcD2R2 set op2
-vcD2R3 set op3
-vcD2R4 set op4
-	endm
-
-; Voices - Decay Level
-smpsVcDecayLevel macro op1,op2,op3,op4
-vcDL1 set op1
-vcDL2 set op2
-vcDL3 set op3
-vcDL4 set op4
-	endm
-
-; Voices - Release Rate
-smpsVcReleaseRate macro op1,op2,op3,op4
-vcRR1 set op1
-vcRR2 set op2
-vcRR3 set op3
-vcRR4 set op4
-	endm
-
-; Voices - Total Level
-smpsVcTotalLevel macro op1,op2,op3,op4
-vcTL1 set op1
-vcTL2 set op2
-vcTL3 set op3
-vcTL4 set op4
-	dc.b	(vcUnusedBits<<6)+(vcFeedback<<3)+vcAlgorithm
-	dc.b	(vcDT4<<4)+vcCF4, (vcDT3<<4)+vcCF3, (vcDT2<<4)+vcCF2, (vcDT1<<4)+vcCF1
-	dc.b	(vcRS4<<6)+vcAR4, (vcRS3<<6)+vcAR3, (vcRS2<<6)+vcAR2, (vcRS1<<6)+vcAR1
-	dc.b	(vcAM4<<5)+vcD1R4, (vcAM3<<5)+vcD1R3, (vcAM2<<5)+vcD1R2, (vcAM1<<5)+vcD1R1
-	dc.b	vcD2R4, vcD2R3, vcD2R2, vcD2R1
-	dc.b	(vcDL4<<4)+vcRR4, (vcDL3<<4)+vcRR3, (vcDL2<<4)+vcRR2, (vcDL1<<4)+vcRR1
-	;dc.b	vcTL4|vcTLMask4, vcTL3|vcTLMask3, vcTL2|vcTLMask2, vcTL1|vcTLMask1
-	if vcAlgorithm=7
-		dc.b	op4|$80
-	else
-		dc.b	op4
-	endif
-
-	if vcAlgorithm>=4
-		dc.b    op3|$80
-	else
-		dc.b    op3
-	endif
-
-	if vcAlgorithm>=5
-		dc.b    op2|$80
-	else
-		dc.b    op2
-	endif
-		dc.b    op1|$80
 	endm
