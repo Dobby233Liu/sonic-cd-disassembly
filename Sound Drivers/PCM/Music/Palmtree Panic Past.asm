09e3e4d6d4dd45ee21ae7efcebf1b0cb91556cdc
Liu Wenyuan <15816141883@163.com>
2023-02-02 15:16:17 +0800

    Revert "fuck You"
    This reverts commit 08bd0520536c0c8cf9ef02d95bea334fd846bcc8.

--- Sound Drivers/PCM/Music/Palmtree Panic Past.asm
+++ Sound Drivers/PCM/Music/Palmtree Panic Past.asm
@@ -16,7 +16,9 @@ PalmtreePanicPast_PCM1:
 	smpsAlterNote	$20
 
 PalmtreePanicPast_Jump1:
-	smpsStop
+	dc.b	nC2, $5e
+	dc.b	nRst, $05
+	smpsJump	PalmtreePanicPast_Jump1
 
 PalmtreePanicPast_PCM2:
 	smpsSetvoice	sBass
